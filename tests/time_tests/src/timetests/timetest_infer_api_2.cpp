8fc58079eb3f5efd2b361ff637cfee97130d70bb
Mateusz Tabaka <mateusz.tabaka@intel.com>
2023-05-11 21:19:25 +0000

    fix time tests

--- tests/time_tests/src/timetests/timetest_infer_api_2.cpp
+++ tests/time_tests/src/timetests/timetest_infer_api_2.cpp
@@ -113,7 +113,7 @@ int runPipeline(const std::string &model, const std::string &device, const bool 
                     std::vector<ov::Output<const ov::Node>> inputs = exeNetwork.inputs();
                     if (reshape && dataShapes.empty()) {
                         fillTensors(inferRequest, defaultInputs);
-                    } else if (reshape && !dataShapes.empty()) {
+                    } else if (!dataShapes.empty()) {
                         fillTensorsWithSpecifiedShape(inferRequest, inputs, dataShapes);
                     } else {
                         fillTensors(inferRequest, inputs);
