6f4180b892f16ff4f7a5688a458782ed600aef35
Mateusz Tabaka <mateusz.tabaka@intel.com>
2023-08-23 10:12:51 +0200

    Remove useless Slices

--- src/common/transformations/src/transformations/common_optimizations/optimize_strided_slice.cpp
+++ src/common/transformations/src/transformations/common_optimizations/optimize_strided_slice.cpp
@@ -14,14 +14,15 @@
 #include "openvino/op/result.hpp"
 #include "openvino/op/slice.hpp"
 #include "openvino/op/strided_slice.hpp"
+#include "openvino/op/slice.hpp"
 #include "openvino/op/util/sub_graph_base.hpp"
 #include "openvino/op/variadic_split.hpp"
 #include "transformations/op_conversions/convert_slice_to_strided_slice.hpp"
 
 using namespace ov;
 
-bool ov::pass::UselessStridedSliceEraser::run_on_model(const std::shared_ptr<ngraph::Function>& f) {
-    RUN_ON_FUNCTION_SCOPE(UselessStridedSliceEraser);
+bool ov::pass::UselessSliceEraser::run_on_model(const std::shared_ptr<ngraph::Function>& f) {
+    RUN_ON_FUNCTION_SCOPE(UselessSliceEraser);
     bool rewritten = false;
     for (auto& node : f->get_ordered_ops()) {
         // Recursively apply transformation for sub-graph based operations
@@ -30,19 +31,19 @@ bool ov::pass::UselessStridedSliceEraser::run_on_model(const std::shared_ptr<ngr
                 rewritten |= run_on_model(sub_graph);
             }
         }
-        auto ss = std::dynamic_pointer_cast<ov::op::v1::StridedSlice>(node);
-        if (!ss || ss->get_output_partial_shape(0).is_dynamic() || ss->get_input_partial_shape(0).is_dynamic())
+        bool is_slice = ov::is_type<ov::op::v1::StridedSlice>(node) || ov::is_type<ov::op::v8::Slice>(node);
+        if (!is_slice || node->get_output_partial_shape(0).is_dynamic() || node->get_input_partial_shape(0).is_dynamic())
             continue;
-        if (ss->input(0).get_shape() != ss->output(0).get_shape())
+        if (node->input(0).get_shape() != node->output(0).get_shape())
             continue;
 
-        auto stridesNode = std::dynamic_pointer_cast<ov::op::v0::Constant>(ss->input_value(3).get_node_shared_ptr());
+        auto stridesNode = std::dynamic_pointer_cast<ov::op::v0::Constant>(node->input_value(3).get_node_shared_ptr());
         if (stridesNode) {
             auto strides = stridesNode->cast_vector<int64_t>();
             if (!std::any_of(strides.begin(), strides.end(), [](int64_t strd) {
                     return strd < 0;
                 }))
-                rewritten |= replace_output_update_name(ss->output(0), ss->input_value(0));
+                rewritten |= replace_output_update_name(node->output(0), node->input_value(0));
         }
     }
     return rewritten;
@@ -403,7 +404,7 @@ bool ov::pass::StridedSliceOptimization::run_on_model(const std::shared_ptr<ngra
 
     bool rewritten = false;
     if (m_use_shapes) {
-        rewritten = UselessStridedSliceEraser().run_on_model(f);
+        rewritten = UselessSliceEraser().run_on_model(f);
         // Execution of other passes is also needed even if 'rewritten' is already 'true'
         rewritten = SharedStridedSliceEraser().run_on_model(f) || rewritten;
         rewritten = GroupedStridedSliceOptimizer().run_on_model(f) || rewritten;
